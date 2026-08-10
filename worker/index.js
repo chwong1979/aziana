57547859f904a3a9eaed9bc794012d4609f91758
Chin-hung Wong <chwong1979@gmail.com>
2026-08-10 16:46:24 -0400

    Localize Aziana catering page

--- worker/index.js
+++ worker/index.js
@@ -3,7 +3,7 @@ import {
   hashSessionId,
 } from './notifications.js';
 
-const VERSION = '0.6.0';
+const VERSION = '0.6.1';
 const ADVISOR_URL = 'https://ai.odarius.com/public/advisor';
 const MAX_ADVISOR_BYTES = 32 * 1024;
 
